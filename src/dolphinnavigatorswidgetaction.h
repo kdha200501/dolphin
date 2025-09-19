d898ac90a0c9cce3ec81d6d0a8beb642ce4ef64f
Jacks Diao <jacks.diao@agilysys.com>
2025-09-19 13:35:32 -0400

    jacks-customizations: ensure one focus at a time

--- src/dolphinnavigatorswidgetaction.h
+++ src/dolphinnavigatorswidgetaction.h
@@ -107,6 +107,8 @@ protected:
     void deleteWidget(QWidget *widget) override;
 
 private:
+  QObject *m_parent;
+  
     /**
      * In Left-to-right languages the Primary side will be the left one.
      */
